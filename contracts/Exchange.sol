cb6ab194df16f10b3cd14bd37911b3019626d60b
Latricia Nickelberry <56142330+nickatnight23@users.noreply.github.com>
2022-08-24 12:45:43 -0700

    12. Fill orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -13,11 +13,13 @@ contract Exchange {
    mapping(uint256 => _Order) public orders;
    uint256 public orderCount;
    mapping(uint256 => bool) public orderCancelled;
+   mapping(uint256 => bool) public orderFilled;
 
    event Deposit(address token, address user, uint256 amount, uint256 balance);
    event Withdraw(address token,address user,uint256 amount, uint256 balance);
    event Order(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
    event Cancel(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+   event Trade(uint256 id, address user, address tokenGet,uint256 amountGet,address tokenGive,uint256 amountGive, address creator,uint256 timestamp);
    // A way to model the order
    struct _Order {
       // Attributes of an order
@@ -66,7 +68,7 @@ contract Exchange {
    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
       //token Give (the token they want to spend)- which token and how much?
    // token Get (the token they want to receive)- which token and how much?
-   orderCount = orderCount +1;
+   orderCount ++;
    // Require token balance
    // Prevent orders if tokens aren't on the exchange
    require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
@@ -94,6 +96,43 @@ contract Exchange {
       orderCancelled[_id] = true;
       // Emit event
       emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, block.timestamp);
+
+   }
+
+   // Executing orders
+
+   function fillOrder(uint256 _id) public {
+         // 1. Must be valid orderId
+         require(_id > 0 && _id <= orderCount,"Order does not exist");
+         // 2. Order cant be filled
+         require(!orderFilled[_id]);
+         // 3. Order cant be cancelled
+         require(!orderCancelled[_id]);
+        // Fetch order
+    _Order storage  _order = orders[_id];
+      // Execute the trade
+      _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+
+      // Mark order as filled
+      orderFilled[_order.id] = true;
+   }
+   function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+      // Fee is paid by the user who filled the order(msg.sender)
+      // Fee is deducted from _amountGet
+      uint256 _feeAmount = (_amountGet * feePercent) / 100;
+      // Execute the trade
+      // msg.sender is the user who filled the order, while _user is who created the order
+      tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+      tokens[_tokenGet][_user] = tokens[_tokenGet][_user]+ _amountGet;
+
+      //charge fees
+      tokens[_tokenGet][feeAccount] =tokens[_tokenGet][feeAccount] + _feeAmount;
+
+      tokens[_tokenGive][_user]=tokens[_tokenGive][_user] - _amountGive;
+      tokens[_tokenGive][msg.sender]=tokens[_tokenGive][msg.sender] + _amountGive;
+
+      // Emit trade event
+      emit Trade(_orderId,msg.sender,_tokenGet,_amountGet,_tokenGive,_amountGive,_user, block.timestamp);
 
    }
 }
