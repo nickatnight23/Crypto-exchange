8c31f0bf32a513d79e46ee0cd8e2448e601ef4ea
Latricia Nickelberry <56142330+nickatnight23@users.noreply.github.com>
2022-09-09 10:09:53 -0700

    20. Deposits pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,51 @@
 import { useEffect } from 'react';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import config from '../config.json';
 
-import { loadProvider, 
-  loadNetwork, 
+import {
+  loadProvider,
+  loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
-}  from '../store/interactions';
+  loadExchange,
+  subscribeToEvents
+} from '../store/interactions';
 
-import Navbar from './Navbar';
-import Markets from './Markets';
-
- 
-
-
-
-
+import Navbar from './Navbar'
+import Markets from './Markets'
+import Balance from './Balance'
 
 function App() {
-
-  const dispatch = useDispatch()  
+  const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-   
-
     // Connect Ethers to blockchain
     const provider = loadProvider(dispatch)
-    // Fetch current network's chainId(e.g hardhat:31337, kovan:42)
+
+    // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
+    const chainId = await loadNetwork(provider, dispatch)
 
     // Reload page when network changes
     window.ethereum.on('chainChanged', () => {
       window.location.reload()
     })
-   
-    // Fetch current account and balance from Metamask
-    const chainId = await loadNetwork(provider, dispatch)
+
+    // Fetch current account & balance from Metamask when changed
     window.ethereum.on('accountsChanged', () => {
-       loadAccount(provider,dispatch)
+      loadAccount(provider, dispatch)
     })
+
     // Load token smart contracts
     const DApp = config[chainId].DApp
     const mETH = config[chainId].mETH
-    await loadTokens(provider,[DApp.address, mETH.address], dispatch)
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch)
 
     // Load exchange smart contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider,exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // Listen to events
+    subscribeToEvents(exchange, dispatch)
   }
 
   useEffect(() => {
@@ -61,9 +60,9 @@ function App() {
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-        <Markets />
+          <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
